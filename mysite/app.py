9adf48f1f2e4b1eaf9581ffbd47d6ed4cb38238b
VovanBruins <thunderlight19@gmail.com>
2025-08-15 22:55:16 +0700

    upload

--- mysite/app.py
+++ mysite/app.py
@@ -1,6 +1,7 @@
 import os
 from datetime import datetime, timedelta, date
 from flask import Flask, render_template, request, redirect, url_for, session, flash, jsonify
+import sqlite3
 
 from werkzeug.security import generate_password_hash, check_password_hash
 import requests
