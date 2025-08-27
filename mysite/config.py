b1de7d0ae0acd1e303562ef65e73c642299ad2f9
UnicU <161994704+UnicU@users.noreply.github.com>
2025-08-27 11:24:26 +0700

    Доработка отправки уведомлений

--- mysite/config.py
+++ mysite/config.py
@@ -1,12 +1,13 @@
 import requests
 
 TOKEN = "7548080659:AAEWmjL4sEYKZ5wVvgeA2JVM9c_H6aUbB7Q"
-CHAT_ID = "2132792365"  # сюда будут приходить уведомления
+CHAT_IDS = ["2132792365", "75355402"]  # список получателей
 
 def send_telegram_message(text: str):
     url = f"https://api.telegram.org/bot{TOKEN}/sendMessage"
-    payload = {"chat_id": CHAT_ID, "text": text}
-    try:
-        requests.post(url, data=payload)
-    except Exception as e:
-        print(f"Ошибка отправки в Telegram: {e}")
+    for chat_id in CHAT_IDS:
+        payload = {"chat_id": chat_id, "text": text}
+        try:
+            requests.post(url, data=payload)
+        except Exception as e:
+            print(f"Ошибка отправки в Telegram {chat_id}: {e}")
